7b7d47c3b63262472d40a13affccdf5cfce95888
220-06\boryungKim <kinobo17@gmail.com>
2025-07-10 12:16:50 +0900

    예외처리 코드 추가

--- src/main/java/org/zerock/domain/SampleDTOList.java
+++ src/main/java/org/zerock/domain/SampleDTOList.java
@@ -6,13 +6,13 @@ import java.util.List;
 import lombok.Data;
 
 @Data
-
 public class SampleDTOList {
-
-	private List<SampleDTO> list; //dto객체를 가지고 있는 리스트
+	
+	private List<SampleDTO> list; // dto 객체를 가지고 있는 리스트
 	
 	public SampleDTOList() {
-		list = new ArrayList<SampleDTO>();
-	} // 생성자
-	//new SampleDTOList() -> 리스트에 dto를 가지고 있는 객체 생성됨
+		 list = new ArrayList<SampleDTO>();
+	} //생성자 
+	// new SampleDTOList() -> 리스트에 dto를 가지고 있는 객체 생성됨.
+	
 }
